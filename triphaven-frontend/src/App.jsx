2ec9fe5bb09f3727a7d0a3cbcdd106d0f954ffe8
lahiruSanjeewa2971 <rathne1997@gmail.com>
2024-12-17 01:57:27 +0530

    Login & registration UI

--- triphaven-frontend/src/App.jsx
+++ triphaven-frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react";
-// import './App.css'
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/Landing";
 import LandingPageCommonLayout from "./components/Landing/common-layout";
+import AuthPage from "./pages/auth";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPageCommonLayout/>} >
+        <Route path="/" element={<AuthPage />} />
+
+        <Route path="/traveller" element={<LandingPageCommonLayout />}>
           <Route path="" element={<LandingPage />} />
-          <Route path="/home" element={<LandingPage />} />
+          {/* <Route path="home" element={<LandingPage />} /> */}
         </Route>
       </Routes>
     </>
---
+++ triphaven-frontend/src/config/index.js
@@ -0,0 +1,40 @@
+export const signInFormControls = [
+    {
+        name: "email",
+        label: "User Email",
+        placeholder: "Enter your email.",
+        type: "email",
+        componentType: "input"
+    },
+    {
+        name: "password",
+        label: "Password",
+        placeholder: "Enter your password.",
+        type: "password",
+        componentType: "input"
+    },
+]
+
+export const signUpFormControls = [
+    {
+        name: "userName",
+        label: "User Name",
+        placeholder: "Enter your user name.",
+        type: "text",
+        componentType: "input"
+    },
+    {
+        name: "email",
+        label: "User Email",
+        placeholder: "Enter your email.",
+        type: "email",
+        componentType: "input"
+    },
+    {
+        name: "password",
+        label: "Password",
+        placeholder: "Enter your password.",
+        type: "password",
+        componentType: "input"
+    },
+]
