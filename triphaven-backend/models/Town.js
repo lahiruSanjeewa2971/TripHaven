f4fadca86d99c9ec74b702782d7827556dcb897b
lahiruSanjeewa2971 <rathne1997@gmail.com>
2025-01-05 23:02:36 +0530

    Towns page - top 3 towns list

--- triphaven-backend/models/Town.js
+++ triphaven-backend/models/Town.js
@@ -4,6 +4,7 @@ const townSchema = new mongoose.Schema({
     townName: { type: String, requied: true, unique: true },
     image: { type: String, required: true },
     description: { type: String },
+    rating: {type: Number, default: 0}
 })
 
 const Town = mongoose.model('Town', townSchema)
