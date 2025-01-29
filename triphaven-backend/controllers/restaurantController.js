fa69382a8db0a377ad3741408718522f0b7f82ee
Hiruni Dewapriya <malshikahiruni99@gmail.com>
2025-01-29 13:22:52 +0530

    latest changes

--- triphaven-backend/controllers/restaurantController.js
+++ triphaven-backend/controllers/restaurantController.js
@@ -1,74 +1,27 @@
-const Restaurant = require('../models/Restaurant');
-const Town = require('../models/Town');
+const express = require("express");
+const {
+  createRestaurant,
+  getFullList,
+  getRestaurantsByTown,
+  getRestaurantsWithTownAdded,
+} = require("../controllers/restaurantController");
+const { authenticate } = require("../middlewares/authMiddleware");
+const { authorizeRole } = require("../middlewares/roleMiddleware");
 
-const createRestaurant = async (req, res) => {
-    try {
-        const { restaurantName, image, cuisine, town, rating, description } = req.body;
+const router = express.Router();
 
-        const checkTown = await Town.findById(town);
-        if (!checkTown) return res.status(404).json({
-            success: false,
-            message: "This town is not found in out system."
-        })
+// restaurantOwner only
+router.post(
+  "/add",
+  authenticate,
+  authorizeRole(["restaurantOwner"]),
+  createRestaurant
+);
+// router.post('/add', createRestaurant)
 
-        const newRestaurant = new Restaurant({
-            restaurantName, image, cuisine, town, rating, description
-        })
-        const saveRestaurant = await newRestaurant.save();
-        if (saveRestaurant) {
-            res.status(200).json({
-                success: true,
-                message: 'New restaurant was added to the list.',
-                data: saveRestaurant
-            })
-        } else {
-            res.status(500).json({
-                success: false,
-                message: 'Something went wrong.'
-            })
-        }
+// common routes
+router.get("/get", getFullList);
+router.get("/get-restaurants-with-town", getRestaurantsWithTownAdded);
+router.get("/get-by-town/:id", getRestaurantsByTown);
 
-    } catch (error) {
-        console.log("Error in createRestaurant :", error)
-        res.status(500).json({
-            success: false,
-            message: "Something went wrong."
-        })
-    }
-}
-
-const getFullList = async (req, res) => {
-    try {
-        const restaurantList = await Restaurant.find();
-
-        res.status(200).json({
-            success: true,
-            data: restaurantList
-        })
-    } catch (error) {
-        console.log('Error in getFullList :', error)
-        res.status(500).json({
-            success: false,
-            message: 'Something went wrong.'
-        })
-    }
-}
-
-const getRestaurantsByTown = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const restaurants = await Restaurant.find({ town: id })
-        res.status(200).json({
-            success: true,
-            data: restaurants
-        })
-    } catch (error) {
-        console.log('Error in getRestaurantsByTown :', error)
-        res.status(500).json({
-            success: false,
-            message: 'Something went wrong.'
-        })
-    }
-}
-
-module.exports = { createRestaurant, getFullList, getRestaurantsByTown }
+module.exports = router;
--- triphaven-backend/routes/restaurantRoutes.js
+++ triphaven-backend/routes/restaurantRoutes.js
@@ -1,16 +1,27 @@
-const express = require('express')
-const { createRestaurant, getFullList, getRestaurantsByTown } = require('../controllers/restaurantController')
-const { authenticate } = require('../middlewares/authMiddleware')
-const { authorizeRole } = require('../middlewares/roleMiddleware')
+const express = require("express");
+const {
+  createRestaurant,
+  getFullList,
+  getRestaurantsByTown,
+  getRestaurantsWithTownAdded,
+} = require("../controllers/restaurantController");
+const { authenticate } = require("../middlewares/authMiddleware");
+const { authorizeRole } = require("../middlewares/roleMiddleware");
 
-const router = express.Router()
+const router = express.Router();
 
 // restaurantOwner only
-router.post('/add', authenticate, authorizeRole(["restaurantOwner"]), createRestaurant)
+router.post(
+  "/add",
+  authenticate,
+  authorizeRole(["restaurantOwner"]),
+  createRestaurant
+);
 // router.post('/add', createRestaurant)
 
 // common routes
-router.get('/get', getFullList)
-router.get('/get-by-town/:id', getRestaurantsByTown)
+router.get("/get", getFullList);
+router.get("/get-restaurants-with-town", getRestaurantsWithTownAdded);
+router.get("/get-by-town/:id", getRestaurantsByTown);
 
-module.exports = router
+module.exports = router;
